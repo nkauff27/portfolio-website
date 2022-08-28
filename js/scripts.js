049428160c649935e263fe35b8a4aca9abf714a2
nkauff27 <111176546+nkauff27@users.noreply.github.com>
2022-08-28 08:25:07 -0400

    Update to page structure and profile image animation
    If applied, this commit will update some html and css as well as change the profile picture and add an animation to it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
 console.log("Hi, my name is Nicole, welcome to my portfolio site")
+
+
+
+var ages = [15, 20, 25, 30];
+
+for (var i=0; i < ages.length; i++) {
+    console.log(ages[i]);
+}
+
+function validate(userInput) {
+	console.log(userInput);
+	if(userInput===""){
+		alert("Whoops, this field cannot be blank!");
+		return false;
+	}
+	else if(userInput==="Bobby"){
+		window.alert("Input cannot be 'Bobby', try again!");
+		return false;
+	}
+	else if(userInput.length>20){
+		window.alert("Input cannot be longer than 20 characters, try again!");
+		return false;			
+		}
+	else{
+		console.log(userInput);
+		window.alert("Thanks for your submission!");
+		return;			
+		}
+	}
