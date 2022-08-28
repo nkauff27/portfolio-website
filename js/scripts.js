6e489a3b60c8d86887a9ec54f5924d48489d2d24
nkauff27 <111176546+nkauff27@users.noreply.github.com>
2022-08-28 14:39:57 -0400

    Adding JQuery
    If applied, these commits will add JQuery interactivity with menu icons and mail button.

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,20 @@ function validate(userInput) {
 		return;			
 		}
 	}
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
+
+document.querySelector('.btn').addEventListener('click', (evt) => {
+    evt.target.classList.add('loading')
+  
+    setTimeout(() => {
+      evt.target.classList.remove('loading')
+    }, 3000);
+  })
